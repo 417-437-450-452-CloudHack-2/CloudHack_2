9294f1dd6ea71b64a389245df59b3fcee27ff48b
shashank-madan <shashank.um.xl@gmail.com>
2022-03-30 01:01:29 +0530

    cleanup

--- worker/app.py
+++ worker/app.py
@@ -10,7 +10,7 @@ print(' [*] Connecting to server ...')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
-channel.queue_declare(queue='ride-match-queue', durable=True)
+# channel.queue_declare(queue='ride-match-queue', durable=True)
 
 print(' [*] Waiting for messages.')
 
@@ -24,14 +24,14 @@ def callback(ch, method, properties, body):
     print(json_obj)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-def callback2(ch, method, properties, body):
-    print(" [x] Received ride match data ")
-    json_str = body
-    print(json_str)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+# def callback2(ch, method, properties, body):
+#     print(" [x] Received ride match data ")
+#     json_str = body
+#     print(json_str)
+#     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='task_queue', on_message_callback=callback)
-channel.basic_consume(queue='ride-match-queue', on_message_callback=callback2)
+# channel.basic_consume(queue='ride-match-queue', on_message_callback=callback2)
 channel.start_consuming()
